b1e5700a65d076326570a445b5d9a8fbac8f05cb
Gomosoft <jgomez@soluntech.com>
2015-01-10 11:42:55 -0500

    inited

---
+++ configuration.php
@@ -0,0 +1,13 @@
+<?php
+$license = 'Leased-ba3b1658b65cc8e75296';
+$db_host = 'localhost';
+$db_username = 'dominets_whmcd15';
+$db_password = '-]QP9Shc41';
+$db_name = 'dominets_whmcd15';
+$cc_encryption_hash = 'yriiknmo8rsbcikik7z0iknqolafumwj1aesaojyxytshvolnvevobkkdlmeq48c';
+$templates_compiledir = '/home/dominets/whmcsdata/templates_c/';
+$attachments_dir = "/home/dominets/whmcsdata/attachments/";
+$downloads_dir = "/home/dominets/whmcsdata/downloads/";
+$customadminpath = "admin";
+$mysql_charset = 'utf8';
+?>
